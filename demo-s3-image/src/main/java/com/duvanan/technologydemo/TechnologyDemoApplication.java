5c04d3b623f8f0b7dd8f94a4dfa71e5c28cc436f
andv49 <andv49@viettel.com.vn>
2024-07-31 14:25:02 +0700

    test

---
+++ demo-s3-image/src/main/java/com/duvanan/technologydemo/TechnologyDemoApplication.java
@@ -0,0 +1,13 @@
+package com.duvanan.technologydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnologyDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechnologyDemoApplication.class, args);
+    }
+
+}
---
+++ demo-s3-image/src/test/java/com/duvanan/technologydemo/TechnologyDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duvanan.technologydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnologyDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
